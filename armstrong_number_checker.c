b652a3e429e49415cccf58f2353441fa859eb7ac
Muhammad Hayat <96942909+Hayatlodhi@users.noreply.github.com>
2023-06-21 10:21:41 +0500

    Create armstrong_no_checker.c
    check whether number is armstorng or not

---
+++ armstrong_number_checker.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+ int pov(int no,int po)
+ {  
+     int i,rzlt;
+     rzlt=no;
+     for(i=1;i<po;i++)
+     rzlt=no*rzlt;
+     return rzlt;
+ }
+
+int main()
+{
+  int a,b,len;
+  int no=153;
+  a=b=no;
+  int newa=0;
+  while(b!=0)
+  {
+      b/=10;
+      len++;
+  }
+   int arr_size=len-1;
+  int arr[arr_size];
+  int i=0;
+  int rem;
+  while(a!=0)
+  {
+      rem=a%10;
+      a/=10;
+      arr[i]=rem;
+      i++;
+  }
+  for(i=0;i<len;i++)
+  {
+      newa=newa+pov(arr[i],len);
+  }
+  if(newa==no)
+  printf("number is armstrong");
+  else 
+  printf("number is not armstrong");
+}
+ 
